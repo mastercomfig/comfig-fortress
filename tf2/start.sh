18950f80a4dbf88875720eab5fc8683035476df6
mastercoms <mastercoms@tuta.io>
2020-05-09 13:01:38 -0400

    initial configs

---
+++ tf2/start.sh
@@ -0,0 +1,1 @@
+server/srcds_run -console -game tf +sv_pure 2 -replay +maxplayers 32 +sv_setsteamaccount +tv_enable 1 +map cp_dustbowl
---
+++ update.sh
@@ -0,0 +1,1 @@
+steamcmd +login anonymous +force_install_dir /opt/tf2_srv/tf2/server +app_update 232250 validate +quit
