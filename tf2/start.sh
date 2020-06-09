31ac4c14d2f63bef8db3428e18dd49af0715388c
mastercoms <mastercoms@tuta.io>
2020-06-09 13:24:14 -0400

    ADD ENV VAR

--- tf2/start.sh
+++ tf2/start.sh
@@ -1,1 +1,1 @@
-server/srcds_run -console -reuse -game tf +sv_pure 1 +maxplayers 32 +sv_setsteamaccount +map cp_dustbowl
+server/srcds_run -console -reuse -game tf +sv_pure 1 +maxplayers 32 +sv_setsteamaccount $TF2_TOKEN +map cp_dustbowl
