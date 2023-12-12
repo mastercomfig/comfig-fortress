273c4c8b10c86024d7636306bf80200792d776f8
mcoms <mastercoms@tuta.io>
2023-12-11 22:59:43 -0500

    new start params

--- tf2/start.sh
+++ tf2/start.sh
@@ -1,1 +1,4 @@
-server/srcds_run -console -reuse -game tf +sv_pure 1 +maxplayers 32 +sv_setsteamaccount $TF2_TOKEN +map cp_dustbowl
+export STEAM_GAMESERVER_A2S_INFO_STRICT_LEGACY_PROTOCOL=0
+export STEAM_GAMESERVER_RATE_LIMIT_200MS=35
+export STEAM_GAMESERVER_PACKET_HANDLER_NO_IPC=1
+server/srcds_run -console -reuse -game tf +sv_pure 1 +maxplayers 32 +sv_setsteamaccount $TF2_TOKEN +map cp_dustbowl -enablefakeip
